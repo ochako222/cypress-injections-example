e75a77d0be3dd4bce16c177dbbf6a2c1edb8da75
Oleksandr Chako <oleksandr.chako@xyleme.com>
2023-05-21 23:09:45 +0200

    WIP

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,33 +1,18 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'cypress';
 
-const path = require('path');
-
 export default defineConfig({
-    reporter: 'cypress-multi-reporters',
-    chromeWebSecurity: false,
-    screenshotsFolder: 'report/assets',
-    screenshotOnRunFailure: true,
-    video: false,
     e2e: {
-        experimentalOriginDependencies: true,
-        experimentalInteractiveRunEvents: true,
-        downloadsFolder: 'report/downloads',
-        watchForFileChanges: false,
-        defaultCommandTimeout: 60 * 1000,
         setupNodeEvents(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) {
             on('task', {
-                async playwright() {
-                    // const { default: myTest } = await import('./playwright/playwright-test');
-                    const { default: defaultExport } = await import('./playwright/playwright-test');
+                async selenium(testPath:string) {
+                    const { default: defaultExport } = await import(`./selenium/${testPath}`);
                     return defaultExport();
                 }
             });
 
             on('task', {
-                async selenium() {
-                    // const { default: myTest } = await import('./playwright/playwright-test');
-                    const { default: defaultExport } = await import('./selenium/selenium-test');
+                async playwright(testPath:string) {
+                    const { default: defaultExport } = await import(`./playwright/${testPath}`);
                     return defaultExport();
                 }
             });
--- cypress/e2e/wiki.cy.ts
+++ cypress/e2e/wiki.cy.ts
@@ -1,9 +1,17 @@
 describe('Extended tests', () => {
-    it('playwright test', () => {
-        cy.task('playwright');
+    it('cypress test', () => {
+      cy.visit('https://www.wikipedia.org/');
+      cy.get('#searchInput').clear().type('Stranger in a Strange Land');
+      cy.get("button[type='submit']").click();
+  
+      cy.get('h1#firstHeading').should('have.text', 'Stranger in a Strange Land')
+    });
+  
+    it('selenium test', () => {
+      cy.task('selenium','selenium-test');
     });
 
-    it('selenium test', () => {
-        cy.task('selenium');
-    });
-});
+    it('playwright test', () => {
+        cy.task('playwright','playwright-test');
+      });
+  });
